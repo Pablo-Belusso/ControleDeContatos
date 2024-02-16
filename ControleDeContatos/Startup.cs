f30a67d51bafa79ac446ec9a2bc1a9c7e10dadf4
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2024-02-16 14:05:41 -0300

    Commit 42

---
+++ ControleDeContatos/Startup.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ControleDeContatos
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Configure os serviços aqui
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // Adicione middlewares específicos para produção aqui
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            // Adicione o middleware MVC
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
